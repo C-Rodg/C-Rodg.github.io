17d1af126fb6e414f66c2cdecd3c6038c3c8a3e4
Kobaine <rodgersbc@gmail.com>
2017-09-23 13:43:29 -0700

    Completed structure of About page

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,7 +1,90 @@
 import React from "react";
 
 const AboutPage = () => {
-	return <div className="about-page">About content...</div>;
+	return (
+		<div className="about-page">
+			<div className="container">
+				<div className="about-image">
+					<img
+						src={require("../assets/aboutphoto.jpg")}
+						alt="Curtis Rodgers - web developer"
+					/>
+				</div>
+				<div className="row row-intro">
+					<div className="col-title">Me.</div>
+					<div className="col-body">
+						Curtis Rodgers. Visual problem solver. Currently based out of
+						Seattle.
+					</div>
+				</div>
+				<div className="row row-do">
+					<div className="col-title">What I do.</div>
+					<div className="col-body">
+						I am a software engineer with a passion for making things look,
+						feel, and operate better. I love to push boundaries in front-end
+						development to find new answers to old problems and to see what is
+						really possible. My passion for this field seems to grow everyday
+						because I know that I am at my best when I am creating.
+					</div>
+				</div>
+				<div className="row row-skills">
+					<div className="col-title">Skills.</div>
+					<div className="col-body">
+						<div className="sub-row">
+							Fundamentally, JavaScript is the reason web users are able to
+							input information and interact with most websites they visit each
+							day. The web development world changes very quickly. In the past,
+							I have proven my ability to learn and adapt to new tools that are
+							required for the task at hand. Some of my favorite frameworks and
+							tools of late include
+							<strong>Webpack</strong>, <strong>React</strong>,
+							<strong>Redux</strong>, <strong>NodeJS</strong>,
+							<strong>Express</strong>, <strong>AngularJS</strong>,
+							<strong>React Native</strong>, and <strong>Ionic</strong>. I also
+							have a strong understanding of how these tools function and how
+							many of their patterns can be implemented using
+							<strong>vanilla Javascript</strong>.
+						</div>
+						<div className="sub-row">
+							I excel at creating hand-coded, search engine optimized,
+							professional websites using HTML5 & CSS3. One of the aspects of
+							the web that I find most exciting, as you can tell from my
+							<a href="https://codepen.io/crodg/" target="_blank">
+								CodePen
+							</a>
+							or
+							<a href="https://github.com/C-Rodg" target="_blank">
+								GitHub
+							</a>
+							pages, is <strong>CSS3 animations</strong>, which I use to bring
+							my pages to life. My real expertise though, is
+							<strong>responsive web design</strong>, which is a necessity in
+							today's rapidly changing web world. My favorite tools which aid
+							development time and help to create DRY, maintanable code includes
+							the front-end framework, <strong>Bootstrap</strong>; my favorite
+							CSS preprocessor, <strong>Sass</strong>; my favorite build tool,
+							<strong> Webpack</strong>; and my favorite CSS postprocessor,
+							<strong>PostCSS</strong>.
+						</div>
+						<div className="sub-row">
+							Object-oriented programming with <strong>C#</strong>. Database
+							management with <strong>SQL</strong> or <strong>NoSQL</strong>.
+							<strong>Adobe Photoshop</strong>. Version control with{" "}
+							<strong>Git</strong> or <strong>Subversion</strong>.
+							Windows/Mac/Linux operating systems.
+						</div>
+					</div>
+				</div>
+				<div className="row row-connect">
+					<div className="col-title">Connect.</div>
+					<div className="col-body">
+						Curtis Rodgers. Visual problem solver. Currently based out of
+						Seattle.
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default AboutPage;
--- src/components/Main.js
+++ src/components/Main.js
@@ -4,12 +4,14 @@ import { Switch, Route } from "react-router-dom";
 import WelcomePage from "./WelcomePage";
 import ContactPage from "./ContactPage";
 import WorkPage from "./WorkPage";
+import AboutPage from "./AboutPage";
 
 const Main = () => {
 	return (
 		<main>
 			<Switch>
 				<Route exact path="/" component={WelcomePage} />
+				<Route path="/about" component={AboutPage} />
 				<Route path="/contact" component={ContactPage} />
 				<Route path="/work" component={WorkPage} />
 			</Switch>

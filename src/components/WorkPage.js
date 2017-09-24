e63ddf759390cd7c8cd69198ec506f7f079f5db3
Kobaine <rodgersbc@gmail.com>
2017-09-24 11:38:35 -0700

    Completed image tile layout

--- src/components/WorkPage.js
+++ src/components/WorkPage.js
@@ -1,154 +1,27 @@
 import React from "react";
 
-const workImages = [
-	{
-		thumb: require("../assets/work/tileNFL.png"),
-		full: require("../assets/work/full/combine.jpg"),
-		category: "Front-end Development",
-		company: "National Football League",
-		desc:
-			"Designed, then completed the front-end development for a web check-in application to be run on Microsoft Surface's for the NFL Combine. This application allowed for team general managers to track which players were in the informal interview room at a given time."
-	},
-	{
-		thumb: require("../assets/work/tilePrecision.png"),
-		full: require("../assets/work/full/precision.jpg"),
-		category: "Web Development",
-		company: "Precision Body Shop & Detail",
-		desc:
-			"Created a mobile-first, responsive website for Precision Auto Body &amp; Detailing out of San Francisco, CA."
-	},
-	{
-		thumb: require("../assets/work/tileTopCoat.png"),
-		full: require("../assets/work/full/topCoat.png"),
-		category: "Web Design",
-		company: "Top Coat Painting",
-		desc:
-			"Designed and developed a responsive, fast, functional website for TopCoat Painting out of Belmont, CA."
-	},
-
-	{
-		thumb: require("../assets/work/tileExplore.jpg"),
-		full: require("../assets/work/full/explore.png"),
-		category: "Wordpress Development",
-		company: "Explore Festivals",
-		desc:
-			"Created a custom wordpress theme for the growing music & arts company - Explore Festivals."
-	},
-	{
-		thumb: require("../assets/work/tileAarons.png"),
-		full: require("../assets/work/full/aarons.jpg"),
-		category: "Web Design",
-		company: "Aaron's Body Shop",
-		desc:
-			"Met with client to determine needs of the business and then from those meetings I created a visual mockup for a complete redesign of Aaron's Body Shop's website."
-	},
-	{
-		thumb: require("../assets/work/tileMAESC.png"),
-		full: require("../assets/work/full/maesc.jpg"),
-		category: "Web Design",
-		company: "Mid-Atlantic Epilepsy & Sleep Center",
-		desc:
-			"Created a new logo and a responsive, modern-looking website for client. Set up Google Analytics so client could gain a better understanding of their site demographics and use this information to improve marketing efforts."
-	},
-
-	{
-		thumb: require("../assets/work/tileChateau.png"),
-		full: require("../assets/work/full/chateau.jpg"),
-		category: "Web Development",
-		company: "Chateau De Lanz",
-		desc:
-			"Handled the design and development of Chateau D'Lanz's responsive e-commerce site built on the Amazon Web Store platform. Also incorporated an automatic newsletter for promotional incentives using MailChimp."
-	},
-	{
-		thumb: require("../assets/work/tileWaterwells.png"),
-		full: require("../assets/work/full/waterwells.jpg"),
-		category: "Web Development",
-		company: "Phil Brien Water Wells",
-		desc:
-			"Handled the design and development of the website for a small business out of Auburn, N.H. Client wanted a responsive design with an easy built-in method to contact the business."
-	},
-	{
-		thumb: require("../assets/work/tileExplore.png"),
-		full: require("../assets/work/full/exploreMock.jpg"),
-		category: "Web Development",
-		company: "Explore Consulting",
-		desc:
-			"Completed design, implementation, and maintenance of e-commerce stores on the Amazon Web Store platform. Often times, I converted antique websites into a responsive format to provide an excellent user experience for a customer on any type of device."
-	},
-
-	{
-		thumb: require("../assets/work/tileSusies.png"),
-		full: require("../assets/work/full/susies.jpg"),
-		category: "Web Development",
-		company: "Ms. Susie's House",
-		desc:
-			"Managed a wordpress blog called Ms. Susie's House for an EEM-CP based out of Washington, D.C."
-	},
-	{
-		thumb: require("../assets/work/tileRecap.png"),
-		full: require("../assets/work/full/recap.jpg"),
-		category: "Web Development",
-		company: "Seattle Seahawks & Sounders",
-		desc:
-			"Created a visual mockup for the login page of the Seattle Seahawks & Sounder's Recap System page."
-	},
-	{
-		thumb: require("../assets/work/tileScott.png"),
-		full: require("../assets/work/full/carpentry.jpg"),
-		category: "Web Development",
-		company: "Scott's Carpentry",
-		desc:
-			"Designed the company's new logo.  Designed and developed a website for the new business that showcases their work using Javascript plugins such as ResponsiveSlides, mixItUp, and Lightbox."
-	},
-
-	{
-		thumb: require("../assets/work/tileYogi.png"),
-		full: require("../assets/work/full/yogi.jpg"),
-		category: "Photoshop Art",
-		company: "Yogi Goods",
-		desc:
-			"Created a title banner using Adobe Photoshop for Yogi Goods based out of Seattle, WA."
-	},
-	{
-		thumb: require("../assets/work/tileCloud.png"),
-		full: require("../assets/work/full/cloud.jpg"),
-		category: "Logo Design",
-		company: "Curtis' Productions",
-		desc:
-			"Created a title banner with elements of flat design for a personal portfolio page."
-	},
-	{
-		thumb: require("../assets/work/tileMashup.png"),
-		full: require("../assets/work/full/mashup.jpg"),
-		category: "Photoshop Art",
-		company: "Abstract Art Fest",
-		desc: "Abstract digital artwork created to showcase Adobe Photoshop skills."
-	},
-
-	{
-		thumb: require("../assets/work/tilePulse.png"),
-		full: require("../assets/work/full/pulsefull.jpg"),
-		category: "Graphic Design",
-		company: "Central Washington University",
-		desc:
-			"Designed a cover image for Central Washington University's Pulse magazine."
-	},
-	{
-		thumb: require("../assets/work/tileGlow.png"),
-		full: require("../assets/work/full/glowname.jpg"),
-		category: "Photoshop Art",
-		company: "Central Washington University",
-		desc:
-			"This digital artwork was created in a web design course to display Adobe Photoshop skills."
-	}
-];
+const Plus = require("react-feather/dist/icons/plus-square").default;
+import Images from "./Images";
+import "../styles/work.scss";
 
 const WorkPage = () => {
 	return (
 		<div className="work-page">
 			<div className="container">
-				{workImages.map((imgObj, i) => {
-					return <img src={imgObj.thumb} key={i} />;
+				{Images.map((imgObj, i) => {
+					return (
+						<figure className="image-tile" key={i}>
+							<img
+								src={imgObj.thumb}
+								alt={[imgObj.company, imgObj.category].join(" - ")}
+							/>
+							<figcaption>
+								<h3>{imgObj.category}</h3>
+								<span>{imgObj.company}</span>
+								<Plus />
+							</figcaption>
+						</figure>
+					);
 				})}
 			</div>
 		</div>

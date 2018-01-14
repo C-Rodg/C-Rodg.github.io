d095a6b5ac5fea8d61124ce608263bd410ec8581
Kobaine <rodgersbc@gmail.com>
2018-01-14 11:58:17 -0800

    Added subtle background animation

--- src/components/ImageTile.js
+++ src/components/ImageTile.js
@@ -6,12 +6,12 @@ const Plus = require("react-feather/dist/icons/plus-square").default;
 const ImageTile = ({ img, idx, onOpenImage }) => {
 	return (
 		<figure className="image-tile" onClick={() => onOpenImage(idx)}>
-			<img src={img.thumb} alt={[img.company, img.category].join(" - ")} />
 			<figcaption>
 				<h3>{img.category}</h3>
 				<span>{img.company}</span>
 				<Plus />
 			</figcaption>
+			<img src={img.thumb} alt={[img.company, img.category].join(" - ")} />
 		</figure>
 	);
 };
